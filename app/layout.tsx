39bfa4d5d010a79d37a2326e4b2b2ccdec5061ee
eldaniga <eldanyga@gmail.com>
2024-06-02 19:35:05 +0200

    New changes abot info

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import Header from "@/components/header";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TarreDev Landing Page",
-  description: "Landing page made by TarreDev",
+  title: "EldanygaDev Landing Page",
+  description: "Landing page made by Eldanyga",
 };
 
 export default function RootLayout({
--- data.tsx
+++ data.tsx
@@ -1,4 +1,4 @@
-import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech, Github } from "lucide-react";
 
 export const socialNetworks = [
     {
@@ -9,20 +9,25 @@ export const socialNetworks = [
     {
         id: 2,
         logo: <Linkedin size={30} strokeWidth={1} />,
-        src: "#!",
+        src: "https://linkedin.com/in/eldanyga",
     },
     {
         id: 3,
+        logo: <Github size={30} strokeWidth={1} />,
+        src: "https://github.com/eldaniga",
+    },
+    {
+        id: 4,
         logo: <Twitter size={30} strokeWidth={1} />,
         src: "#!",
     },
     {
-        id: 4,
+        id: 5,
         logo: <Rss size={30} strokeWidth={1} />,
         src: "#!",
     },
     {
-        id: 5,
+        id: 6,
         logo: <Twitch size={30} strokeWidth={1} />,
         src: "#!",
     },
@@ -65,59 +70,59 @@ export const itemsNavbar = [
 export const dataAboutPage = [
     {
         id: 1,
-        title: "Frontend Developer",
-        subtitle: "TechSolutions",
-        description: "Colabora con un equipo dinámico para desarrollar interfaces de usuario atractivas y funcionales que impulsen el éxito de nuestros clientes en el mundo digital.",
-        date: "Nov 2023 ",
-    },
-    {
-        id: 2,
-        title: "Creador de Experiencias Digitales",
-        subtitle: "PixelCrafters",
-        description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
-        date: "May 2021",
+        title: "Frontend Developer & Junior Full Stack Developer",
+        subtitle: "SAT",
+        description: "Creación de proyectos basados en React y Next.js. Destaca un Software que estoy desarrollando para profesores de Universidad",
+        date: "May 2024 ",
     },
     {
         id: 3,
-        title: "Especialista en Desarrollo Frontend",
-        subtitle: "CodeForge Solutions",
-        description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
-        date: "Ago 2019",
+        title: "Especialista en Redes Cisco",
+        subtitle: "Cisco",
+        description: "Obtención de las 3 Certificaciones de Cisco CCNA1, CCNA2, CCNA3",
+        date: "May 2024",
     },
     {
         id: 4,
-        title: "Prácticas Grado",
-        subtitle: "WebWizards Inc.",
-        description: "Únete a nosotros mientras creamos sitios web y aplicaciones interactivas que sorprenden y deleitan a nuestros clientes. Si tienes pasión por el diseño y la programación, y disfrutas colaborar en un entorno creativo, ¡queremos conocerte!        ",
-        date: "Mar 2018",
+        title: "Ganador de Beca Talento",
+        subtitle: "Universidad de Jaen",
+        description: "Merecedor de una dotación económica valorada en casi 3000 euros anuales para comenzar y completar mis estudios en Ingeniería en Telecomunicaciones",
+        date: "Sep 2022",
     },
+    {
+        id: 2,
+        title: "Comienzo en el Mundo de la Programación",
+        subtitle: "Autodidacta",
+        description: "Comienzo en mundo de la programación, que siempre me apasionó, programando por primera vez en Python ",
+        date: "May 2018",
+    }
 ]
 
 export const dataCounter = [
     {
         id: 0,
-        endCounter: 10,
+        endCounter: 2,
         text: "Años de experiencia",
         lineRight: true,
         lineRightMobile: true,
     },
     {
         id: 1,
-        endCounter: 80,
+        endCounter: 2,
         text: "Clientes satisfechos",
         lineRight: true,
         lineRightMobile: false,
     },
     {
         id: 2,
-        endCounter: 220,
+        endCounter: 12,
         text: "Proyectos finalizados",
         lineRight: true,
         lineRightMobile: true,
     },
     {
         id: 3,
-        endCounter: 30,
+        endCounter: 1,
         text: "Premios ganadores",
         lineRight: false,
         lineRightMobile: false,
